8a896ca65aa448a05af03b6b5981f08af6320e6a
Allen Baker <althebaker43@gmail.com>
2015-05-25 17:39:59 -0700

    Added better error handling and messaging in Main.

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,14 @@
 #include "RedBot.h"
 #include <iostream>
 
+#define MAX_ERROR_COUNT 5
+
+/**
+ * Writes an error message for an invalid received packet
+ */
+void WriteIncoherentMessage(
+        const RedBot& robot
+        );
 
 int
 WPIRBMain(
@@ -25,17 +33,69 @@ WPIRBMain(
 
     robot.modeInit(FieldControlSystem::MODE_DISABLED);
     std::cout << "Program: beginning loop." << std::endl;
-    int status = 0;
-    while(true)
+    size_t errorCount = 0;
+    while (errorCount < MAX_ERROR_COUNT)
     {
         robot.modePeriodic(FieldControlSystem::MODE_DISABLED);
-        if (robot.isConnected() == false)
+
+        if (robot.getStatus() != RedBot::STATUS_GOOD)
         {
-            std::cerr << "Error: robot lost connection." << std::endl;
-            status = 1;
-            break;
+            switch (robot.getStatus())
+            {
+                case RedBot::STATUS_DISCONNECTED:
+                    std::cerr << "Error: robot lost connection." << std::endl;
+                    break;
+
+                case RedBot::STATUS_INCOHERENT:
+                    WriteIncoherentMessage(robot);
+                    robot.resync();
+                    break;
+
+                case RedBot::STATUS_UNRESPONSIVE:
+                    std::cerr << "Error: robot is unresponsive." << std::endl;
+                    robot.resync();
+                    break;
+
+                default:
+                    break;
+            }
+
+            ++errorCount;
+        }
+        else
+        {
+            // Reset error count
+            errorCount = 0;
         }
     }
 
-    return status;
+    return (errorCount >= MAX_ERROR_COUNT);
+}
+
+void
+WriteIncoherentMessage(
+        const RedBot& robot
+        )
+{
+    std::list<std::string> lastRequestPackets;
+    std::list<std::string> lastResponsePackets;
+
+    robot.getLastBinaryTransaction(
+            lastRequestPackets,
+            lastResponsePackets
+            );
+    const std::string& invalidPacket = lastResponsePackets.back();
+
+    std::cerr << "Error: invalid packet received: ";
+
+    for(
+            size_t charIdx = 0;
+            charIdx < invalidPacket.length();
+            ++charIdx
+       )
+    {
+        std::cerr << std::hex << (unsigned int)invalidPacket[0] << ' ';
+    }
+
+    std::cerr << std::endl;
 }
