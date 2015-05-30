25eef372a03b5b43c393ebc6be4273e64bd30396
Allen Baker <althebaker43@gmail.com>
2015-05-30 11:43:32 -0700

    Added simple driving sample.

---
+++ samples/DriveStraight/Robot.cpp
@@ -0,0 +1,43 @@
+#include "WPILib.h"
+#include <stdio.h>
+
+class Robot : public IterativeRobot
+{
+    RobotDrive myDrive;
+    Timer myDriveTimer;
+    bool myIsDrivingForward;
+
+    public:
+
+        Robot() :
+            myDrive(0, 0),
+            myDriveTimer(),
+            myIsDrivingForward(true)
+        {
+        }
+
+    private:
+
+        void RobotInit()
+        {
+        }
+
+        void DisabledInit()
+        {
+            myDriveTimer.Start();
+            myDrive.Drive(1.0, 0.0);
+        }
+
+        void DisabledPeriodic()
+        {
+            if (myDriveTimer.HasPeriodPassed(5.0) == true)
+            {
+                myDriveTimer.Reset();
+                myIsDrivingForward = !myIsDrivingForward;
+                myDrive.Drive((myIsDrivingForward ? 1.0 : -1.0), 0.0);
+                printf("Program: driving %s\n", (myIsDrivingForward ? "forward" : "backward"));
+            }
+        }
+};
+
+START_ROBOT_CLASS(Robot);
